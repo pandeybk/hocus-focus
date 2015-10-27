68b25fd6ce1452c485c1ed58377d755f35cee1b2
Balkrishna Pandey <sachit.nep@gmail.com>
2015-10-27 10:35:24 +0545

    php related

--- cookbooks/starof_customers/recipes/webserver.rb
+++ cookbooks/starof_customers/recipes/webserver.rb
@@ -22,12 +22,19 @@ directory node['starof_customers']['document_root'] do
     recursive true
 end
 
+# Load the secrets file and the encrypted data bag item that holds the database password.
+password_secret = Chef::EncryptedDataBagItem.load_secret(node['starof_customers']['passwords']['secret_path'])
+user_password_data_bag_item = Chef::EncryptedDataBagItem.load('passwords', 'db_admin_password', password_secret)
+
 # Write a default home page.
-file "#{node['starof_customers']['document_root']}/index.php" do
-    content '<html> Hello world to Chef! </html>'
+template "#{node['starof_customers']['document_root']}/index.php" do
+    source 'index.php.erb'
     mode '0644'
     owner node['starof_customers']['user']
     group node['starof_customers']['group']
+    variables({
+        :database_password => user_password_data_bag_item['password']
+        })
 end
 
 # Open port 80 to incoming traffic.
@@ -35,4 +42,15 @@ firewall_rule 'http' do
   port 80
   protocol :tcp
   action :allow
+end
+
+# Install the mod_php5 Apache module.
+#httpd_module 'php5' do
+#  instance 'customers'
+#end
+
+# Install php5-mysql.
+package 'php5-mysql' do
+  action :install
+  notifies :restart, 'httpd_service[customers]'
 end
